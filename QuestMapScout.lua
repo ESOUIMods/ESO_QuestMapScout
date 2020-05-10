9c2a6b37496c6175d126afb65cf447ef9b350039
Sharlikran <danielhmpdx16@gmail.com>
2020-05-09 23:42:13 -0700

    Update to record more data and the questID
    questID can be obtained when turning in the quest or when abandoning a quest.

--- QuestMapScout.lua
+++ QuestMapScout.lua
@@ -16,6 +16,7 @@ local lastZone
 if QM_Scout == nil then QM_Scout = {startTime=GetTimeStamp()} end
 local GPS = LibGPS2
 local LMP = LibMapPins
+local quest_shared
 
 -- Check if zone is base zone
 local function IsBaseZone(zoneAndSubzone)
@@ -37,6 +38,18 @@ end
 
 -- Event handler function for EVENT_QUEST_ADDED
 local function OnQuestAdded(eventCode, journalIndex, questName, objectiveName)
+    if quest_shared then
+        quest_shared = false
+        return
+    end
+    -- -1 for these means it is undefined since 0 is a valid value
+    local quest_type = -1
+    local repeat_type = -1
+    local quest_id = -1
+    -- -1 means it has not been completed. Once complete set it to the time stamp
+    -- I am considering using this to see if I can calculate order for Zone quests
+    local time_completed = -1
+
     -- Add quest to saved variables table in correct zone element
     if QM_Scout.quests == nil then QM_Scout.quests = {} end
     local zone = LMP:GetZoneAndSubzone(true)
@@ -44,20 +57,28 @@ local function OnQuestAdded(eventCode, journalIndex, questName, objectiveName)
     local normalizedX, normalizedY = GetMapPlayerPosition("player")
     local gpsx, gpsy, zoneMapIndex = GPS:LocalToGlobal(normalizedX, normalizedY)
     local measurement = GPS:GetCurrentMapMeasurements()
+    if journalIndex then
+        quest_type = GetJournalQuestType(journalIndex)
+        repeat_type = GetJournalQuestRepeatType(journalIndex)
+    end
     local quest = {
-            ["name"]      = questName,
-            ["x"]         = normalizedX,
-            ["y"]         = normalizedY,
-            ["gpsx"]      = gpsx,
-            ["gpsy"]      = gpsy,
-            ["giver"]     = questGiverName,
-            ["otherInfo"] = {
-                    ["time"]            = GetTimeStamp(),
-                    ["api"]             = GetAPIVersion(),
-                    ["lang"]            = GetCVar("language.2"),
-                    ["measurement"]     = measurement,
-                },
-        }
+        ["quest_type"]  = GetJournalQuestType(journalIndex),
+        ["repeat_type"] = GetJournalQuestRepeatType(journalIndex),
+        ["name"]        = questName,
+        ["x"]           = normalizedX,
+        ["y"]           = normalizedY,
+        ["gpsx"]        = gpsx,
+        ["gpsy"]        = gpsy,
+        ["giver"]       = questGiverName,
+        ["questID"]     = quest_id, -- assign this and get the ID when the quest is removed
+        ["otherInfo"] = {
+            ["time_completed"]  = time_completed,
+            ["api"]             = GetAPIVersion(),
+            ["lang"]            = GetCVar("language.2"),
+            ["measurement"]     = measurement,
+        },
+    }
+
     if not string.find(string.lower(questGiverName), "crafting writ") then
         if QuestMap then
             local quest_not_found = true
@@ -74,7 +95,7 @@ local function OnQuestAdded(eventCode, journalIndex, questName, objectiveName)
                     quest_not_found = false
                 end
             end
-            if quest_not_found then 
+            if quest_not_found then
                 table.insert(QM_Scout.quests[zone], quest)
             end
         else
@@ -82,14 +103,21 @@ local function OnQuestAdded(eventCode, journalIndex, questName, objectiveName)
         end
     end
 end
+EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_QUEST_ADDED, OnQuestAdded) -- Verified
 
 -- Event handler function for EVENT_CHATTER_END
 local function OnChatterEnd(eventCode)
     reward = nil
     -- Stop listening for the quest added event because it would only be for shared quests
-    EVENT_MANAGER:UnregisterForEvent(AddonName, EVENT_QUEST_ADDED)
-    EVENT_MANAGER:UnregisterForEvent(AddonName, EVENT_CHATTER_END)
+    -- Shar I added if EVENT_QUEST_SHARED to OnQuestAdded UnregisterForEvent EVENT_QUEST_ADDED
+    -- EVENT_MANAGER:UnregisterForEvent(AddonName, EVENT_QUEST_ADDED)
+    EVENT_MANAGER:UnregisterForEvent(AddonName, EVENT_CHATTER_END) -- Verified
 end
+
+local function OnQuestSharred(eventCode, questID)
+    quest_shared = true
+end
+EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_QUEST_SHARED, OnQuestSharred) -- Verified
 
 -- Event handler function for EVENT_QUEST_OFFERED
 local function OnQuestOffered(eventCode)
@@ -97,10 +125,11 @@ local function OnQuestOffered(eventCode)
     -- (This could also be done in OnQuestAdded directly, but it's saver here because we are sure the dialogue is open)
     questGiverName = GetUnitName("interact")
     -- Now that the quest has ben offered we can start listening for the quest added event
-    EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_QUEST_ADDED, OnQuestAdded)
-    EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_CHATTER_END, OnChatterEnd)
+    -- Shar I added if EVENT_QUEST_SHARED to OnQuestAdded UnregisterForEvent EVENT_QUEST_ADDED
+    -- EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_QUEST_ADDED, OnQuestAdded)
+    EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_CHATTER_END, OnChatterEnd) -- Verified
 end
-EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_QUEST_OFFERED, OnQuestOffered)
+EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_QUEST_OFFERED, OnQuestOffered) -- Verified
 
 -- Event handler function for EVENT_QUEST_COMPLETE_DIALOG
 local function OnQuestCompleteDialog(eventCode, journalIndex)
@@ -112,25 +141,60 @@ local function OnQuestCompleteDialog(eventCode, journalIndex)
         table.insert(reward, rewardType)
     end
 end
-EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_QUEST_COMPLETE_DIALOG, OnQuestCompleteDialog)
+EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_QUEST_COMPLETE_DIALOG, OnQuestCompleteDialog) -- Verified
 
 -- Event handler function for EVENT_QUEST_REMOVED
 local function OnQuestRemoved(eventCode, isCompleted, journalIndex, questName, zoneIndex, poiIndex, questID)
-    if not isCompleted then return end
-    -- Save quest repeat and reward type
-    if not QM_Scout.questInfo then QM_Scout.questInfo = {} end
-    if not QM_Scout.questInfo[questID] then QM_Scout.questInfo[questID] = {} end
-    QM_Scout.questInfo[questID].repeatType = GetJournalQuestRepeatType(journalIndex)
-    QM_Scout.questInfo[questID].rewardTypes = reward
-    reward = nil
+    local quest_to_update = {}
+
+    for zone, zone_quests in pairs(QM_Scout["quests"]) do
+        for num_entry, quest_from_table in pairs(zone_quests) do
+            if quest_from_table.name == questName then
+                quest_to_update = quest_from_table
+                break
+            end
+        end
+    end
+
+    if not isCompleted  then
+        return
+    end
+    if quest_to_update == nil then
+        return
+    end
+
+    if quest_to_update.questID == -1 then
+        quest_to_update.questID = questID
+    end
+
+    if isCompleted then
+        quest_to_update["otherInfo"].time_completed = GetTimeStamp()
+
+        if not QM_Scout.questInfo then QM_Scout.questInfo = {} end
+        if not QM_Scout.questInfo[questID] then QM_Scout.questInfo[questID] = {} end
+        QM_Scout.questInfo[questID].repeatType = GetJournalQuestRepeatType(journalIndex)
+        QM_Scout.questInfo[questID].rewardTypes = reward
+        reward = nil
+    end
 end
-EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_QUEST_REMOVED, OnQuestRemoved)
+EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_QUEST_REMOVED, OnQuestRemoved) -- Verified
 
 -- Event handler function for EVENT_PLAYER_DEACTIVATED
 local function OnPlayerDeactivated(eventCode)
     lastZone = LMP:GetZoneAndSubzone(true)
 end
-EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_PLAYER_DEACTIVATED, OnPlayerDeactivated)
+EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_PLAYER_DEACTIVATED, OnPlayerDeactivated) -- Verified
+
+function show_quests()
+    for zone, zone_quests in pairs(QM_Scout.quests) do
+        d("zone: "..zone)
+        for num_entry, quest_from_table in pairs(zone_quests) do
+            d("Quest From Table Name: "..quest_from_table.name)
+            quest_to_update = quest_from_table
+            d("Quest To Update Name: "..quest_to_update.name)
+        end
+    end
+end
 
 -- Event handler function for EVENT_PLAYER_ACTIVATED
 local function OnPlayerActivated(eventCode)
@@ -145,18 +209,21 @@ local function OnPlayerActivated(eventCode)
             local gpsx, gpsy, gpsm = GPS:LocalToGlobal(x, y)
             local measurement = GPS:GetCurrentMapMeasurements()
             QM_Scout.subZones[zone][lastZone] = {
-                    -- previously this was reversed ?? GetMapPlayerPosition
-                    -- won't reverse that ??
-                    -- ["y"] = x,
-                    -- ["x"] = y,
-                    ["x"] = x,
-                    ["y"] = y,
-                    ["gpsx"] = gpsx,
-                    ["gpsy"] = gpsy,
-                    ["measurement"] = measurement,
-                }
+                -- previously this was reversed ?? GetMapPlayerPosition
+                -- won't reverse that ??
+                -- ["y"] = x,
+                -- ["x"] = y,
+                ["x"] = x,
+                ["y"] = y,
+                ["gpsx"] = gpsx,
+                ["gpsy"] = gpsy,
+                ["measurement"] = measurement,
+            }
         end
     end
     lastZone = zone
+
+    SLASH_COMMANDS["/qmslist"] = show_quests
+
 end
-EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_PLAYER_ACTIVATED, OnPlayerActivated)
+EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_PLAYER_ACTIVATED, OnPlayerActivated) -- Verified
